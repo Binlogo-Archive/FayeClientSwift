f4cef681f77b5828e684668ab0bd575a829eb1d4
Binboy <binboy@live.com>
2017-05-25 19:31:25 +0800

    add faye server node example

---
+++ faye_server_node_example/faye_publish.js
@@ -0,0 +1,10 @@
+var faye = require('faye'),
+    client = new faye.Client('http://localhost:5222/faye');
+
+
+faye.Logging.logLevel = 'debug';
+
+client.publish("/cool", {"text": "ok from server nice stuff"})
+client.publish("/awesome", {"text": "awesome from server nice stuff"})
+
+console.log("Message sent!");
